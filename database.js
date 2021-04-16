72fa52696cb255820bbe24d3ef5c81c999103efb
Nadia Rosenberg <nadia>
2021-04-16 12:04:25 -0300

    no gitignore

--- database.js
+++ database.js
@@ -14,6 +14,12 @@ con.connect(function(err) {
   console.log("Connected!");
 });
 
+//Drop table 
+var sql = "DROP TABLE inputs";
+con.query(sql, function (err, result) {
+  if (err) throw err;
+  console.log("Table dropped");
+});
 
 //Create table
 var sql = "CREATE TABLE IF NOT EXISTS inputs (input_id int NOT NULL AUTO_INCREMENT, concept VARCHAR(25), amount decimal(12) NOT NULL, input_date DATE NOT NULL, input_type VARCHAR(20) NOT NULL, type_expense VARCHAR(10), PRIMARY KEY (input_id))";
--- javatpoint.sql
+++ javatpoint.sql
@@ -1,3 +1,2 @@
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '12345';
-FLUSH PRIVILEGES; 
-
+/*ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '12345';
+FLUSH PRIVILEGES; */
