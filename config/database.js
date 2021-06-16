157d4a55ccca50025865b2c0d25723f357adcc4e
Nadia Rosenberg <nadia>
2021-06-16 18:03:10 -0300

    Adds test data

--- config/database.js
+++ config/database.js
@@ -16,11 +16,29 @@ con.connect(function(err) {
   console.log("Connected!");
 });
 
-var sql = "CREATE TABLE IF NOT EXISTS inputs (input_id int NOT NULL AUTO_INCREMENT, concept VARCHAR(25), amount decimal(12) NOT NULL, input_date DATE NOT NULL, input_type VARCHAR(20) NOT NULL, type_expense VARCHAR(10), PRIMARY KEY (input_id))";
+var sql = "CREATE TABLE IF NOT EXISTS inputs (id int NOT NULL AUTO_INCREMENT, concept VARCHAR(25), amount decimal(12) NOT NULL, input_date DATE NOT NULL, input_type VARCHAR(20) NOT NULL, type_expense VARCHAR(10), PRIMARY KEY (id))";
 con.query(sql, function (err, result) {
     if (err) throw err;
     if(result.warningCount == 0){
       console.log("Table created");
+    }
+});
+
+//Test data
+var sql = "SELECT * from inputs";
+con.query(sql, function (err, result) {
+    if (err) throw err;
+    if(result.length == 0){
+      var sql = "INSERT INTO inputs (concept, amount, input_date, input_type, type_expense) VALUES ?";
+      var values = [
+        ['Alquiler', 20000, '2021-04-21', 'Expense', 'Living'],
+        ['Comida', 500, '2021-04-21', 'Expense', 'Food'],
+        ['Venta', 5000, '2021-04-21', 'Income', null]
+      ];
+      con.query(sql, [values], function (err, result) {
+          if (err) throw err;
+          console.log("3 rows added");
+      });
     }
 });
 
